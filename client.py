c6af137d8220ddd1034ae4134139bac11ace4d35
KelNoMel <68493100+KelNoMel@users.noreply.github.com>
2021-11-15 22:59:02 +1100

    Changed Templates

--- client.py
+++ client.py
@@ -1,31 +1,53 @@
-#coding: utf-8
+"""
+    Modified from template by Wei Song
+    Python 3
+    Usage: python3 TCPClient3.py localhost 12000
+    coding: utf-8
+    
+    Author: Kellen Liew
+
+"""
 from socket import *
+import sys
 
-#Define connection (socket) parameters
-#Address + Port no
 #Server would be running on the same host as Client
-serverName = 'localhost'
+if len(sys.argv) != 3:
+    print("\n===== Error usage, python3 TCPClient3.py SERVER_IP SERVER_PORT ======\n");
+    exit(0);
+serverHost = sys.argv[1]
+serverPort = int(sys.argv[2])
+serverAddress = (serverHost, serverPort)
 
-#change this port number if required
-serverPort = 12000
-
+# define a socket for the client side, it would be used to communicate with the server
 clientSocket = socket(AF_INET, SOCK_STREAM)
-#This line creates the client’s socket. The first parameter indicates the address family; in particular,AF_INET indicates that the underlying network is using IPv4. The second parameter indicates that the socket is of type SOCK_STREAM,which means it is a TCP socket (rather than a UDP socket, where we use SOCK_DGRAM). 
 
-clientSocket.connect((serverName, serverPort))
-#Before the client can send data to the server (or vice versa) using a TCP socket, a TCP connection must first be established between the client and server. The above line initiates the TCP connection between the client and server. The parameter of the connect( ) method is the address of the server side of the connection. After this line of code is executed, the three-way handshake is performed and a TCP connection is established between the client and server.
+# build connection with the server and send message to it
+clientSocket.connect(serverAddress)
 
-sentence = input('Input lowercase sentence:')
-#input() is a built-in function in Python. When this command is executed, the user at the client is prompted with the words “Input lowercase sentence:” The user then uses the keyboard to input a line, which is put into the variable sentence. Now that we have a socket and a message, we will want to send the message through the socket to the destination host.
+while True:
+    message = input("===== Please type any messsage you want to send to server: =====\n")
+    clientSocket.sendall(message.encode())
 
-clientSocket.send(sentence.encode('utf-8'))
-#As the connection has already been established, the client program simply drops the bytes in the string sentence into the TCP connection. Note the difference between UDP sendto() and TCP send() calls. In TCP we do not need to attach the destination address to the packet, as was the case with UDP sockets.
+    # receive response from the server
+    # 1024 is a suggested packet size, you can specify it as 2048 or others
+    data = clientSocket.recv(1024)
+    receivedMessage = data.decode()
 
-modifiedSentence = clientSocket.recv(1024)
-#We wait to receive the reply from the server, store it in modifiedSentence
+    # parse the message received from server and take corresponding actions
+    if receivedMessage == "":
+        print("[recv] Message from server is empty!")
+    elif receivedMessage == "user credentials request":
+        print("[recv] You need to provide username and password to login")
+    elif receivedMessage == "download filename":
+        print("[recv] You need to provide the file name you want to download")
+    else:
+        print("[recv] Message makes no sense")
+        
+    ans = input('\nDo you want to continue(y/n) :')
+    if ans == 'y':
+        continue
+    else:
+        break
 
-print('From Server:', modifiedSentence.decode('utf-8'))
-#print what we have received
-
+# close the socket
 clientSocket.close()
-#and close the socket
--- server.py
+++ server.py
@@ -1,36 +1,93 @@
-#coding: utf-8
+"""
+    Modified from template by Wei Song
+    Python 3
+    Usage: python3 TCPClient3.py localhost 12000
+    coding: utf-8
+    
+    Author: Kellen Liew
+"""
 from socket import *
-#using the socket module
+from threading import Thread
+import sys, select
 
-#Define connection (socket) parameters
-#Address + Port no
-#Server would be running on the same host as Client
-# change this port number if required
-serverPort = 12000 
+# acquire server host and port from command line parameter
+if len(sys.argv) != 2:
+    print("\n===== Error usage, python3 TCPServer3.py SERVER_PORT ======\n");
+    exit(0);
+serverHost = "127.0.0.1"
+serverPort = int(sys.argv[1])
+serverAddress = (serverHost, serverPort)
 
+# define socket for the server side and bind address
 serverSocket = socket(AF_INET, SOCK_STREAM)
-#This line creates the server’s socket. The first parameter indicates the address family; in particular,AF_INET indicates that the underlying network is using IPv4.The second parameter indicates that the socket is of type SOCK_STREAM,which means it is a TCP socket (rather than a UDP socket, where we use SOCK_DGRAM).
+serverSocket.bind(serverAddress)
 
-serverSocket.bind(('localhost', serverPort))
-#The above line binds (that is, assigns) the port number 12000 to the server’s socket. In this manner, when anyone sends a packet to port 12000 at the IP address of the server (localhost in this case), that packet will be directed to this socket.
+"""
+    Define multi-thread class for client
+    This class would be used to define the instance for each connection from each client
+    For example, client-1 makes a connection request to the server, the server will call
+    class (ClientThread) to define a thread for client-1, and when client-2 make a connection
+    request to the server, the server will call class (ClientThread) again and create a thread
+    for client-2. Each client will be runing in a separate therad, which is the multi-threading
+"""
+class ClientThread(Thread):
+    def __init__(self, clientAddress, clientSocket):
+        Thread.__init__(self)
+        self.clientAddress = clientAddress
+        self.clientSocket = clientSocket
+        self.clientAlive = False
+        
+        print("===== New connection created for: ", clientAddress)
+        self.clientAlive = True
+        
+    def run(self):
+        message = ''
+        
+        while self.clientAlive:
+            # use recv() to receive message from the client
+            data = self.clientSocket.recv(1024)
+            message = data.decode()
+            
+            # if the message from client is empty, the client would be off-line then set the client as offline (alive=Flase)
+            if message == '':
+                self.clientAlive = False
+                print("===== the user disconnected - ", clientAddress)
+                break
+            
+            # handle message from the client
+            if message == 'login':
+                print("[recv] New login request")
+                self.process_login()
+            elif message == 'download':
+                print("[recv] Download request")
+                message = 'download filename'
+                print("[send] " + message)
+                self.clientSocket.send(message.encode())
+            else:
+                print("[recv] " + message)
+                print("[send] Cannot understand this message")
+                message = 'Cannot understand this message'
+                self.clientSocket.send(message.encode())
+    
+    """
+        You can create more customized APIs here, e.g., logic for processing user authentication
+        Each api can be used to handle one specific function, for example:
+        def process_login(self):
+            message = 'user credentials request'
+            self.clientSocket.send(message.encode())
+    """
+    def process_login(self):
+        message = 'user credentials request'
+        print('[send] ' + message)
+        self.clientSocket.send(message.encode())
 
-serverSocket.listen(1)
-#The serverSocket then goes in the listen state to listen for client connection requests. 
 
-print("The server is ready to receive")
+print("\n===== Server is running =====")
+print("===== Waiting for connection request from clients...=====")
 
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-#When a client knocks on this door, the program invokes the accept( ) method for serverSocket, which creates a new socket in the server, called connectionSocket, dedicated to this particular client. The client and server then complete the handshaking, creating a TCP connection between the client’s clientSocket and the server’s connectionSocket. With the TCP connection established, the client and server can now send bytes to each other over the connection. With TCP, all bytes sent from one side not are not only guaranteed to arrive at the other side but also guaranteed to arrive in order
 
-    sentence = connectionSocket.recv(1024)
-#wait for data to arrive from the client
-
-    capitalizedSentence = sentence.upper()
-#change the case of the message received from client
-
-    connectionSocket.send(capitalizedSentence)
-#and send it back to client
-
-    connectionSocket.close()
-#close the connectionSocket. Note that the serverSocket is still alive waiting for new clients to connect, we are only closing the connectionSocket.
+while True:
+    serverSocket.listen()
+    clientSockt, clientAddress = serverSocket.accept()
+    clientThread = ClientThread(clientAddress, clientSockt)
+    clientThread.start()
